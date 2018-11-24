801e45491657ed4c763c834411ee28bc4dbe5d8b
Orkhan Orujaliyev <o.orkhan@gmail.com>
2018-11-24 17:10:03 +0400

    bootstrap card worked

--- resources/views/department/show.blade.php
+++ resources/views/department/show.blade.php
@@ -6,29 +6,34 @@
 @section('content')
 
 <div class="container-fluid">
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+    <li class="breadcrumb-item">
+        <a href="/">Dashboard</a>
+    </li>
+    <li class="breadcrumb-item active">@yield('title')</li>
+    </ol>
 
-<!-- Breadcrumbs-->
-<ol class="breadcrumb">
-<li class="breadcrumb-item">
-    <a href="/">Dashboard</a>
-</li>
-<li class="breadcrumb-item active">@yield('title')</li>
-</ol>
-
-<div class="row">
-    <div class="col-md-8">
-        <div class="panel panel-primary">
-            <div class="panel-heading"><h1>{{$department->name}} department info.</h1></div>
-            <hr>
-            <div class="panel-body">
-                <p>{{$department->description}}</p>
-            </div>
-        </div>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card mb-3">
+                <div class="card-header">
+                    <i class="fas fa-table"></i>
+                    {{$department->name}} department info.
+                </div>
+                <div class="card-body">
+                    <h2>Description:</h2>
+                    <p>{{$department->description}}</p>
+                </div>
+                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+            </div>    
+        </div>        
     </div>
+    <a href="{{ URL::previous() }}">Go Back</a>
 </div>
 
 
 
 <!-- /.container-fluid -->
-<a href="{{ URL::previous() }}">Go Back</a>
+
 @endsection
